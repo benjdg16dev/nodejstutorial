35605303f795aa75fe6a33db4c3787c69bdf428f
benjdg16dev <benjdg16dev@gmail.com>
2023-02-05 16:25:38 -0500

    [feat] tutorial#4 event emitter

---
+++ 04tut/logEvents.js
@@ -0,0 +1,28 @@
+// NPM modules
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+// Common core modules
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+
+  // Note: Append file creates the file but not the directory
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      logItem
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = logEvents;
