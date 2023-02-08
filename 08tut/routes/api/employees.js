b24fa08e88d0a3e471f9c0b23c53f2062f8d8644
benjdg16dev <benjdg16dev@gmail.com>
2023-02-08 13:06:11 -0500

    [feat] attempt on updating file

--- 08tut/routes/api/employees.js
+++ 08tut/routes/api/employees.js
@@ -6,22 +6,61 @@ const router = express.Router();
 const path = require("path");
 const data = {};
 
+const fs = require("fs");
+const filePath = path.join(__dirname, "..", "..", "data", "employees.json");
+
 // Note: Just like connecting the path to Database
-data.employees = require(path.join("..", "..", "data", "employees.json"));
+data.employees = require(filePath);
+
+const fsData = fs.readFileSync(filePath, {
+  encoding: "utf8",
+});
 
 router
   .route("/")
   .get((req, res) => {
-    res.json(data.employees);
+    // res.json(data.employees);
+
+    // Note: Assignment attempt
+    if (!!fsData) {
+      res.json(JSON.parse(fsData));
+    }
   })
   .post((req, res) => {
     // Note: Not yet a full api
     // Just showing that we can use parameters in this post request
-    // This will NOT put into the data file
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
+    // his will NOT put into the data file
+
+    // fs.res.json({
+    //   firstname: req.body.firstname,
+    //   lastname: req.body.lastname,
+    // });
+
+    // Note: Assignment attempt
+
+    if (!!req.body.firstname && !!req.body.lastname) {
+      // Note: Allows duplicates; will append as new ID
+      const currentData = JSON.parse(
+        fs.readFileSync(filePath, { encoding: "utf8" })
+      );
+
+      const id = Array.isArray && currentData?.length;
+
+      currentData.push({
+        id,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+      });
+
+      fs.writeFileSync(filePath, JSON.stringify(currentData));
+      const newData = fs.readFileSync(filePath, { encoding: "utf8" });
+
+      res.json(JSON.parse(newData));
+    } else {
+      res.status(400).send({
+        message: "Error 400: Bad Request",
+      });
+    }
   })
   .put((req, res) => {
     res.json({
